023b52ff7c563d298706faab6f6f915e9a77dfd3
Ahmed M. Araby <ahmed@araby.name>
2016-03-18 17:04:45 +0200

    fix in ars technia

--- grep/spiders/arstechnica.py
+++ grep/spiders/arstechnica.py
@@ -1,17 +1,17 @@
 __author__ = 'ahmed'
-from time import gmtime, strftime
+import traceback
+import sys
 import scrapy
-from scrapy import log
+import logging
 from grep.items import GrepItem
-from scrapy.contrib.linkextractors import LinkExtractor
-from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
-from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 
 
 class ArstechnicaSpider(CrawlSpider):
 
     #####################
-    ## scrappy own vars
+    # # scrappy own vars
     ######################
     name = "arstechnica"
     allowed_domains = ["arstechnica.com"]
@@ -24,16 +24,18 @@ class ArstechnicaSpider(CrawlSpider):
         Rule(LinkExtractor(allow=('/[\-0-9a-zA-Z]+/\d+/\d+/[\-0-9a-zA-Z]+/'), unique=True),
              callback='parse_article'),
         # # rules to allow categories only
-        Rule(SgmlLinkExtractor(allow=('/[\-0-9a-zA-Z]+/'), unique=True))
+        Rule(LinkExtractor(allow=('/[\-0-9a-zA-Z]+/'), unique=True))
     )
 
 
-    ## my own variables
-
+    #######################
+    # # my own variables
+    #######################
     xpaths = {
         'title': '//h1[@itemprop="headline"]//text()',
         'image': '//div[@itemprop="articleBody"]//figure[contains(@class,"intro-image")]//img/@src',
-        'content': '//div[@itemprop="articleBody"]//p/text()'
+        'content': '//div[@itemprop="articleBody"]//p/text()',
+        'time': '//p[@itemprop="author creator"]//span[@class="date"]/@data-time'
     }
 
     response = None
@@ -41,18 +43,20 @@ class ArstechnicaSpider(CrawlSpider):
     def parse_article(self, response):
         self.response = response
 
-        item = GrepItem()
-        item.url = response.url
-        item.title = self.getxPath(self.xpaths['title'])[0]
-        item.image = self.getxPath(self.xpaths['image'])[0]
-        item.content =
-        pass
+        try:
+            item = GrepItem()
+            item['url'] = self.response.url
+            item['title'] = self.getxPath(self.xpaths['title'])[0]
+            item['image'] = self.getxPath(self.xpaths['image'])[0]
+            item['time'] = self.getxPath(self.xpaths['time'])[0]
+            content = ''
+            for singleContent in self.getxPath(self.xpaths['content']):
+                content += singleContent
+            item['content'] = content
+            return [item]
+        except Exception, e:
+            traceback.print_exc(file=sys.stderr)
+            self.log(" Url failed ", logging.ERROR)
 
     def getxPath(self, selectXpath):
         return self.response.xpath(selectXpath).extract()
-
-    def log(self, message, level=log.INFO):
-        if type(self.response) is scrapy.http.response.html.HtmlResponse:
-            message = self.response.url + " " + message
-        msg = strftime("%Y-%m-%d %H:%M:%S", gmtime()) + " " + message
-        return CrawlSpider.log(self, msg, level)
